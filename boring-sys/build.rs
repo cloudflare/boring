5c6caf7812c30069519d7174755bfb72e3f09b92
ilammy <me@ilammy.net>
2021-12-30 19:52:39 +0900

    boring-sys: Configure "rustc-cdylib-link-arg" only for macOS targets
    cfg!() is evaluated for the host OS executing build.rs script.
    What we need here is to look whether we are building *for* macOS.
    
    Otherwise, for example, builds for iOS on macOS will try to add this
    flag, causing warnings since rustc does not build cdylibs on iOS.

--- boring-sys/build.rs
+++ boring-sys/build.rs
@@ -354,7 +354,8 @@ fn main() {
     println!("cargo:rustc-link-lib=static=ssl");
 
     // MacOS: Allow cdylib to link with undefined symbols
-    if cfg!(target_os = "macos") {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target_os == "macos" {
         println!("cargo:rustc-cdylib-link-arg=-Wl,-undefined,dynamic_lookup");
     }
 
