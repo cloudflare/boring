9219cbfeeab580a4b98131618c1060189dccd7f4
Jordan Rose <jrose@signal.org>
2022-06-02 13:36:08 -0700

    boring-sys: Save time by not building the bssl CLI utility
    We only need libcrypto and libssl.

--- boring-sys/build.rs
+++ boring-sys/build.rs
@@ -263,7 +263,8 @@ fn main() {
             cfg.define("FIPS", "1");
         }
 
-        cfg.build_target("bssl").build().display().to_string()
+        cfg.build_target("ssl").build();
+        cfg.build_target("crypto").build().display().to_string()
     });
 
     let build_path = get_boringssl_platform_output_path();
