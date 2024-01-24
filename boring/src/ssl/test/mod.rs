69d40839ccbc381efa1a4465d42de0f49a2cd0a4
Isaiah Becker-Mayer <isaiah@goteleport.com>
2024-01-24 10:29:10 -0800

    Gate compliance policy apis: The SSL_CTX_set_compliance_policy and ssl_compliance_policy_t apis are not available in the fips validated hash of the boringssl library (boring-sys/deps/boringssl-fips). This adds back the feature gate for these apis.

--- boring/src/ssl/test/mod.rs
+++ boring/src/ssl/test/mod.rs
@@ -21,6 +21,7 @@ use crate::ssl::{
 use crate::x509::verify::X509CheckFlags;
 use crate::x509::{X509Name, X509};
 
+#[cfg(not(feature = "fips"))]
 use super::CompliancePolicy;
 
 mod custom_verify;
@@ -947,6 +948,7 @@ fn test_get_ciphers() {
 }
 
 #[test]
+#[cfg(not(feature = "fips"))]
 fn test_set_compliance() {
     let mut ctx = SslContext::builder(SslMethod::tls()).unwrap();
     ctx.set_compliance_policy(CompliancePolicy::FIPS_202205)
