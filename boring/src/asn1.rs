3ac871d977ff846056883cf9724df0ab71c5ef5a
southorange0929 <southorange0929@foxmail.com>
2024-04-01 16:44:29 +0800

    docs: add docs

--- boring/src/asn1.rs
+++ boring/src/asn1.rs
@@ -317,6 +317,8 @@ impl Asn1Time {
         ffi::init();
 
         unsafe {
+            // for higher musl version, need to convert i32 to i64
+            // https://github.com/rust-lang/libc/issues/1848
             let handle = cvt_p(ffi::ASN1_TIME_set(ptr::null_mut(), time.into()))?;
             Ok(Asn1Time::from_ptr(handle))
         }
