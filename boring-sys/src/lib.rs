222135bfee47521daa46c42a4f6e92acaeec17fa
Rushil Mehra <rmehra@cloudflare.com>
2024-07-31 00:52:31 -0700

    Allow dead_code instead of disabling clippy entirely for bindgen

--- boring-sys/src/lib.rs
+++ boring-sys/src/lib.rs
@@ -16,9 +16,11 @@ use std::convert::TryInto;
 use std::ffi::c_void;
 use std::os::raw::{c_char, c_int, c_uint, c_ulong};
 
-#[allow(dead_code)]
-#[allow(clippy::all)]
-#[rustfmt::skip]
+#[allow(
+    clippy::useless_transmute,
+    clippy::derive_partial_eq_without_eq,
+    dead_code
+)]
 mod generated {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
