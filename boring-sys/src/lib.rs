7cb4c895506952f42c9452ba420d99edc658f643
Kornel <kornel@cloudflare.com>
2026-01-30 14:44:49 +0000

    Detect bad headers in boring-sys

--- boring-sys/src/lib.rs
+++ boring-sys/src/lib.rs
@@ -24,6 +24,15 @@ use std::os::raw::{c_char, c_int, c_uint, c_ulong};
 mod generated {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
+
+// explicitly require presence of some symbols to check if the bindings worked
+pub use generated::{ssl_compliance_policy_t, ERR_add_error_data, SSL_set1_groups}; // if these are missing, your include path is incorrect or has a wrong version of boringssl
+pub use generated::{BIO_new, OPENSSL_free, SSL_ERROR_NONE}; // if these are missing, your include path is incorrect
+#[cfg(feature = "fips")]
+pub use generated::{FIPS_mode, SSL_CTX_set_compliance_policy}; // your include path is incorrect or has a version of boringssl without FIPS support
+#[cfg(feature = "rpk")]
+pub use generated::{SSL_CREDENTIAL_new_raw_public_key, SSL_CREDENTIAL_set1_spki}; // your include path is incorrect or has a version of boringssl without rpk support
+
 pub use generated::*;
 
 #[cfg(target_pointer_width = "64")]
